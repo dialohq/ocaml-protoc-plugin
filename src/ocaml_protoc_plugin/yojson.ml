a206b6bad0518b20987b037ec4318cbd1a1156d1
Paul Tsnobiladzé <paul.tsnobiladze@gmail.com>
2022-11-24 15:53:13 +0100

    add of_yojson for extensions and field

--- src/ocaml_protoc_plugin/yojson.ml
+++ src/ocaml_protoc_plugin/yojson.ml
@@ -9,3 +9,18 @@ type t =
   | `List of t list
   | `Tuple of t list
   | `Variant of string * t option ]
+
+  module Safe = struct 
+    type t = [ 
+    | `Null
+    | `Bool of bool
+    | `Int of int
+    | `Intlit of string
+    | `Float of float
+    | `String of string
+    | `Assoc of (string * t) list
+    | `List of t list
+    | `Tuple of t list
+    | `Variant of string * t option
+    ]
+  end 
